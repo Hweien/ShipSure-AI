1edf4412a8aba3369286f444abf37819bd465070
Kristy2124 <haukristy2124@gmail.com>
2026-09-21 15:19:01 +0800

    Modify so Analytic able to obtain data

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -62,7 +62,7 @@ export const Header: React.FC<HeaderProps> = ({
   return (
     <header
       id="shipsure-header"
-      className="h-16 bg-white border-b border-slate-200 px-6 flex items-center justify-between sticky top-0 z-30 shadow-xs"
+      className="h-16 bg-white border-b border-slaSte-200 px-6 flex items-center justify-between sticky top-0 z-30 shadow-xs"
     >
       {/* Left: Brand Identity */}
       <div className="flex items-center gap-4">
